54c9e3aed501e12df5f0ecb66f8bbc49d724448d
kazuyama-4 <howholic.royalky420@gmail.com>
2021-05-26 18:48:20 +0900

    食材管理コントローラー、view,routes

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -1,4 +1,6 @@
 class ManagementsController < ApplicationController
   def index
   end
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'managements#index'
+  resources :managements, only: %i[index new]
 end
